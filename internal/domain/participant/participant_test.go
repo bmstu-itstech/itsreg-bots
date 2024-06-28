381d58310aaaa0cddad682b360dd86e3976f44b4
zhikh <zhikh.k@gmail.com>
2024-06-28 19:05:33 +0300

    feat: add participant struct

---
+++ internal/domain/participant/participant_test.go
@@ -0,0 +1,21 @@
+package participant_test
+
+import (
+	"github.com/stretchr/testify/require"
+	"github.com/zhikh23/itsreg-tgservice/internal/domain/participant"
+	"testing"
+)
+
+func TestParticipant_New(t *testing.T) {
+	t.Run("successfully create a new participant", func(t *testing.T) {
+		part, err := participant.New(42)
+		require.NoError(t, err)
+		require.NotNil(t, part)
+		require.Equal(t, int32(42), part.Id)
+	})
+
+	t.Run("fails to create a participant with invalid id", func(t *testing.T) {
+		_, err := participant.New(0)
+		require.ErrorIs(t, err, participant.ErrInvalidParticipantId)
+	})
+}
