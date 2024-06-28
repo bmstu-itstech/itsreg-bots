0b90041fcc2bb11a9f00f3e9f27f9ef6a6cbf079
zhikh <zhikh.k@gmail.com>
2024-06-28 23:19:47 +0300

    ref: change pointer types to integer ids

--- internal/domain/module/module_test.go
+++ internal/domain/module/module_test.go
@@ -8,11 +8,12 @@ import (
 
 func TestModule_New(t *testing.T) {
 	type args struct {
+		id       int32
 		title    string
 		text     string
 		isSilent bool
 		typ      module.Type
-		next     *module.Module
+		next     int32
 		buttons  []module.Button
 	}
 
@@ -32,57 +33,63 @@ func TestModule_New(t *testing.T) {
 		{
 			test: "created common module successfully",
 			args: args{
+				id:       1,
 				title:    "title",
 				text:     "text",
 				isSilent: false,
 				typ:      module.String,
-				next:     &module.Module{},
+				next:     2,
 				buttons:  nil,
 			},
 			res: &module.Module{
+				Id:       1,
 				Title:    "title",
 				Text:     "text",
 				IsSilent: false,
 				Type:     module.String,
-				Next:     &module.Module{},
+				Next:     2,
 				Buttons:  make([]module.Button, 0),
 			},
 		},
 		{
 			test: "created module with buttons successfully",
 			args: args{
+				id:       1,
 				title:    "title",
 				text:     "text",
 				isSilent: false,
 				typ:      module.String,
-				next:     &module.Module{},
+				next:     2,
 				buttons:  []module.Button{{}},
 			},
 			res: &module.Module{
+				Id:       1,
 				Title:    "title",
 				Text:     "text",
 				IsSilent: false,
 				Type:     module.String,
-				Next:     &module.Module{},
+				Next:     2,
 				Buttons:  []module.Button{{}},
 			},
 		},
 		{
 			test: "created silent module successfully",
 			args: args{
+				id:       1,
 				title:    "title",
 				text:     "text",
 				isSilent: true,
 				typ:      module.String,
-				next:     &module.Module{},
+				next:     3,
 				buttons:  nil,
 			},
 			res: &module.Module{
+				Id:       1,
 				Title:    "title",
 				Text:     "text",
 				IsSilent: true,
 				Type:     module.String,
-				Next:     &module.Module{},
+				Next:     3,
 				Buttons:  make([]module.Button, 0),
 			},
 		},
@@ -90,13 +97,7 @@ func TestModule_New(t *testing.T) {
 
 	for _, pc := range posCases {
 		t.Run(pc.test, func(t *testing.T) {
-			mod, errs := module.New(
-				pc.title,
-				pc.text,
-				pc.isSilent,
-				pc.typ,
-				pc.next,
-				pc.buttons)
+			mod, errs := module.New(pc.id, pc.title, pc.text, pc.isSilent, pc.typ, pc.next, pc.buttons)
 			require.Nil(t, errs)
 			require.NotNil(t, mod)
 			require.Equal(t, pc.res.Title, mod.Title)
@@ -112,11 +113,12 @@ func TestModule_New(t *testing.T) {
 		{
 			test: "error to create module with empty title",
 			args: args{
+				id:       1,
 				title:    "",
 				text:     "text",
 				isSilent: true,
 				typ:      module.String,
-				next:     &module.Module{},
+				next:     2,
 				buttons:  make([]module.Button, 0),
 			},
 			errs: []error{
@@ -126,11 +128,12 @@ func TestModule_New(t *testing.T) {
 		{
 			test: "error to create module with empty text",
 			args: args{
+				id:       1,
 				title:    "title",
 				text:     "",
 				isSilent: true,
 				typ:      module.String,
-				next:     &module.Module{},
+				next:     2,
 				buttons:  make([]module.Button, 0),
 			},
 			errs: []error{
@@ -140,11 +143,12 @@ func TestModule_New(t *testing.T) {
 		{
 			test: "error to create module with invalid type",
 			args: args{
+				id:       1,
 				title:    "title",
 				text:     "text",
 				isSilent: true,
 				typ:      0,
-				next:     &module.Module{},
+				next:     2,
 				buttons:  make([]module.Button, 0),
 			},
 			errs: []error{
@@ -154,11 +158,12 @@ func TestModule_New(t *testing.T) {
 		{
 			test: "error to create silent module with buttons",
 			args: args{
+				id:       1,
 				title:    "title",
 				text:     "text",
 				isSilent: true,
 				typ:      module.Number,
-				next:     &module.Module{},
+				next:     2,
 				buttons:  []module.Button{{}},
 			},
 			errs: []error{
@@ -168,11 +173,12 @@ func TestModule_New(t *testing.T) {
 		{
 			test: "error to create last module with buttons",
 			args: args{
+				id:       1,
 				title:    "title",
 				text:     "text",
 				isSilent: false,
 				typ:      module.Number,
-				next:     nil,
+				next:     0,
 				buttons:  []module.Button{{}},
 			},
 			errs: []error{
@@ -183,13 +189,7 @@ func TestModule_New(t *testing.T) {
 
 	for _, nc := range negCases {
 		t.Run(nc.test, func(t *testing.T) {
-			_, errs := module.New(
-				nc.title,
-				nc.text,
-				nc.isSilent,
-				nc.typ,
-				nc.next,
-				nc.buttons)
+			_, errs := module.New(nc.id, nc.title, nc.text, nc.isSilent, nc.typ, nc.next, nc.buttons)
 			require.NotNil(t, errs)
 			require.ElementsMatch(t, nc.errs, errs, "errors are not equal")
 		})
@@ -203,7 +203,7 @@ func TestModule_IsLast(t *testing.T) {
 			Text:     "text",
 			IsSilent: true,
 			Type:     module.String,
-			Next:     nil,
+			Next:     0,
 			Buttons:  make([]module.Button, 0),
 		}
 		require.True(t, mod.IsLast())
@@ -215,7 +215,7 @@ func TestModule_IsLast(t *testing.T) {
 			Text:     "text",
 			IsSilent: true,
 			Type:     module.String,
-			Next:     &module.Module{},
+			Next:     1,
 			Buttons:  make([]module.Button, 0),
 		}
 		require.False(t, mod.IsLast())
@@ -225,11 +225,12 @@ func TestModule_IsLast(t *testing.T) {
 func TestModule_HasButtons(t *testing.T) {
 	t.Run("module has buttons", func(t *testing.T) {
 		mod := &module.Module{
+			Id:       1,
 			Title:    "title",
 			Text:     "text",
 			IsSilent: true,
 			Type:     module.String,
-			Next:     &module.Module{},
+			Next:     2,
 			Buttons:  []module.Button{{}},
 		}
 		require.True(t, mod.HasButtons())
@@ -237,11 +238,12 @@ func TestModule_HasButtons(t *testing.T) {
 
 	t.Run("module has no buttons", func(t *testing.T) {
 		mod := &module.Module{
+			Id:       1,
 			Title:    "title",
 			Text:     "text",
 			IsSilent: true,
 			Type:     module.String,
-			Next:     &module.Module{},
+			Next:     2,
 			Buttons:  make([]module.Button, 0),
 		}
 		require.False(t, mod.HasButtons())
