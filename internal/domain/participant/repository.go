ce1b30af6c408baf6f7ed15ffed0204175b72931
zhikh <zhikh.k@gmail.com>
2024-07-01 12:31:34 +0300

    feat: add logging to processor

--- internal/domain/participant/repository.go
+++ internal/domain/participant/repository.go
@@ -14,5 +14,5 @@ var (
 type Repository interface {
 	Save(prt entity.Participant) error
 	Get(id entity.ParticipantId) (entity.Participant, error)
-	UpdateCurrentId(id entity.ParticipantId, currentId objects.State) error
+	UpdateState(id entity.ParticipantId, state objects.State) error
 }
