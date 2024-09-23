a5c3755013bbc517b3731beaf9137b46df428f23
zhikh <zhikh.k@gmail.com>
2024-09-23 12:22:21 +0300

    fix: incomplete logging of commands and queries

--- internal/common/decorator/logging.go
+++ internal/common/decorator/logging.go
@@ -14,17 +14,17 @@ type commandLoggingDecorator[C any] struct {
 func (d commandLoggingDecorator[C]) Handle(ctx context.Context, cmd C) (err error) {
 	handlerType := generateActionName(cmd)
 
-	logger := d.logger.
-		With("command", handlerType).
-		With("command_body", fmt.Sprintf("%v", cmd))
+	logger := d.logger.With(
+		slog.String("command", handlerType),
+		slog.String("command_body", fmt.Sprintf("%v", cmd)),
+	)
 
 	logger.Debug("Executing command")
 	defer func() {
 		if err == nil {
 			logger.Info("Command executed successfully")
 		} else {
-			logger.With("error", err.Error()).
-				Error("Failed to execute command")
+			logger.Error("Failed to execute command", "error", err.Error())
 		}
 	}()
 
@@ -37,17 +37,17 @@ type queryLoggingDecorator[C any, R any] struct {
 }
 
 func (d queryLoggingDecorator[C, R]) Handle(ctx context.Context, cmd C) (result R, err error) {
-	logger := d.logger.
-		With("query", generateActionName(cmd)).
-		With("query_body", fmt.Sprintf("%v", cmd))
+	logger := d.logger.With(
+		slog.String("query", generateActionName(cmd)),
+		slog.String("query_body", fmt.Sprintf("%v", cmd)),
+	)
 
 	logger.Debug("Executing query")
 	defer func() {
 		if err == nil {
 			logger.Info("Query executed successfully")
 		} else {
-			logger.With("error", err.Error()).
-				Error("Failed to execute query")
+			logger.Error("Failed to execute query", "error", err.Error())
 		}
 	}()
 
