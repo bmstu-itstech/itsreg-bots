43e0ab3d294bac046a796ebfa7d146f2acec9858
zhikh <zhikh.k@gmail.com>
2024-11-12 18:09:43 +0300

    fix: fix timestamp in pretty logger

--- internal/common/logs/handlers/slogpretty/slogpretty.go
+++ internal/common/logs/handlers/slogpretty/slogpretty.go
@@ -68,7 +68,7 @@ func (h *PrettyHandler) Handle(_ context.Context, r slog.Record) error {
 		}
 	}
 
-	timeStr := r.Time.Format("[15:05:05.000]")
+	timeStr := r.Time.Format("[02/01 15:04:05.000]")
 	msg := color.CyanString(r.Message)
 
 	h.l.Println(
